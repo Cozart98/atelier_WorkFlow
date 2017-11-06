b865827215638d53f31f34844ac2a4f3abdc7cea
Cozart98 <chihaoui1098@gmail.com>
2017-11-06 11:39:39 +0100

    premier envoie

---
+++ app/src/main/java/com/example/apprenti/gitworkflow/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.apprenti.gitworkflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+    }
+}
